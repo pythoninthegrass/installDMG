aaf8f8f86ced2d6b5d6311b6b5ac45038ddf37d7
pythoninthegrass <lstephens@lance-mbp.corp.rhapsody.com>
2016-11-21 16:45:01 -0800

    Cosmetic changes

--- installDMG.sh
+++ installDMG.sh
@@ -1,11 +1,11 @@
 #/bin/sh
 
-## Source:
+# SOURCE:
 # http://stackoverflow.com/a/21428907
 # Originally created by user zmo
-## Usage:
+# USAGE:
 # $ installDMG.sh ~/Downloads/myApp.dmg
-## ToDo:
+# TODO:
 # Error handling for when app already exists in /Applications
 
 loggedInUser=$(ls -l /dev/console | cut -d " " -f 4)
@@ -14,7 +14,7 @@ scriptDir=$(cd "$(dirname "$0")" && pwd)
 installLog="/tmp/volume-dmg-install.log"
 
 # volumeMount=$(hdiutil attach "$1" | grep Volumes | awk '{print $3}') #Original
-volumeMount=$(hdiutil attach "$1" | ls | grep *.app) 
+volumeMount=$(hdiutil attach "$1" | ls | grep *.app)
 cp -rf "$volumeMount/*.app" /Applications
 hdiutil detach $volumeMount
 

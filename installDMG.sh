e02088bb1e6167ab8970ee1941f94f4a23ed75ef
pythoninthegrass <lstephens@lance-mbp.corp.rhapsody.com>
2017-02-20 17:24:01 -0800

    env, ifs, arrays, hdiutil, cp apps
    Tightened env location, set internal field separator (white spaces),
    DMG and volume location arrays, for loop to copy apps if not installed.

--- installDMG.sh
+++ installDMG.sh
@@ -1,21 +1,57 @@
-#/bin/sh
+#!/usr/bin/env bash
 
-# SOURCE:
-# http://stackoverflow.com/a/21428907
-# Originally created by user zmo
-# USAGE:
-# $ installDMG.sh ~/Downloads/myApp.dmg
-# TODO:
-# Error handling for when app already exists in /Applications
 
+# Exit upon failed command
+# set -e
+
+# Install log
+logTime=$(date +%Y-%m-%d:%H:%M:%S)
+installLog="/tmp/pkg_install_$logTime.log"
+exec &> >(tee -a "$installLog")
+
+# Current user
 loggedInUser=$(ls -l /dev/console | cut -d " " -f 4)
-scriptDir=$(cd "$(dirname "$0")" && pwd)
-#echo $scriptDir
-installLog="/tmp/volume-dmg-install.log"
 
-# volumeMount=$(hdiutil attach "$1" | grep Volumes | awk '{print $3}') #Original
-volumeMount=$(hdiutil attach "$1" | ls | grep *.app)
-cp -rf "$volumeMount/*.app" /Applications
-hdiutil detach $volumeMount
+# Working directory
+scriptDir=$(cd "$(dirname "$0")" && pwd)
+
+# Check for root privileges
+if [ $(whoami) != "root" ]; then
+    echo "Sorry, you need super user privileges to run this script."
+    exit 1
+fi
+
+# Set $IFS to eliminate whitespace in pathnames
+IFS="$(printf '\n\t')"
+
+# DMG list to iterate through below
+dmgArray=($(find "$scriptDir" -maxdepth 1 -name "*.dmg" | awk -F/ '{print $NF}'))
+# echo "${dmgArray[@]}"
+
+# Mount DMGs
+for a in "${dmgArray[@]}"; do
+    yes | /usr/bin/hdiutil attach -noautoopen $a
+    echo "$a was mounted successfully."
+done
+
+# List mounted volume directories
+volumeMount=($(hdiutil info | grep Volumes | cut -f 3))
+# echo ${volumeMount[@]}
+
+# List app name
+volumeApp=($(find /Volumes/ -maxdepth 2 -name "*.app" | awk -F/ '{print $NF}'))
+# echo ${volumeApp[@]}
+
+# Loop through app installs # one dmg works
+for APP in "${volumeApp[@]}"; do
+    # echo $APP
+    [ ! -f /Applications/"$APP" ] && cp -rf /Volumes/*/"$APP" /Applications/
+done
+
+# Unmount all DMGs via AppleScript
+osascript -e 'tell application "Finder" to eject (every disk whose ejectable is true and local volume is true and free space is not equal to 0)'
+osascript -e 'tell application "Finder" to close window 1'
+
+unset IFS
 
 exit 0
